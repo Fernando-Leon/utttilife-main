39ac38a207273cee7a5a09d3a256dda1f830b12a
brandon <brandonfuentes025@gmail.com>
2024-02-19 12:42:48 -0600

    Agregados comentarios para los archivos de recipes

--- app/src/main/java/com/example/utttilife/screens/RecipesScreen.kt
+++ app/src/main/java/com/example/utttilife/screens/RecipesScreen.kt
@@ -14,17 +14,25 @@ import com.example.utttilife.components.recipes.MainViewModel
 import com.example.utttilife.views.recipes.CameraImage
 import com.example.utttilife.views.recipes.LayoutContent
 
+// Componente principal de la pantalla
 @Composable
 fun RecipesScreen() {
+    // Obtenemos la instancia del ViewModel
     val viewModel: MainViewModel = viewModel()
+    // Definimos un contenedor Box
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
+        // Guardamos el NavController
         val navController = rememberNavController()
+        // Configuramos el sistema de navegación
         NavHost(navController = navController, startDestination = "main") {
+            // Definimos el destino para la pantalla principal
             composable("main") { LayoutContent(navController, viewModel) }
+            // Definimos el destino para la pantalla de la cámara
             composable("camera") {
+                // Llamamos al componente CameraImage y le pasamos el NavController, la función de captura de imagen del ViewModel y el contexto actual
                 CameraImage(navController, viewModel::onImageCaptured, context = LocalContext.current)
             }
         }
