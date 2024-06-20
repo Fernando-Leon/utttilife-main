d92207d07bdb87891a3f5524e1ce835a47df4b12
ReiLennux <lennyrm343@outlook.com>
2024-06-20 15:10:19 -0600

    factos brandon

--- app/src/main/java/com/example/utttilife/MainActivity.kt
+++ app/src/main/java/com/example/utttilife/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                    // LoginScreen()
                  MainScreen()
-                    //RegisterApartamentScreen()
+                    //suka
                 }
             }
         }
